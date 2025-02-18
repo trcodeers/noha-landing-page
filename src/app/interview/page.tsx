5e109877ca731545ed3d201b33fcd1e712bb2994
Tarun-kumar-singh <tarunks2512@gmail.com>
2025-02-18 12:14:18 +0530

    feat: establish socket connection for audio streaming in interview page

--- src/app/interview/page.tsx
+++ src/app/interview/page.tsx
@@ -3,6 +3,7 @@ import Feedback from "@/components/feedback";
 import InterviewDetails from "@/components/InterviewDetails";
 import LiveInterview from "@/components/LiveInterview";
 import { useState } from "react";
+import { io } from "socket.io-client";
 
 const MyPage = () => {
 
@@ -11,10 +12,28 @@ const MyPage = () => {
 
     const [callended, setCallEnded] = useState(false)
 
+
+    const [backendServiceLink] = useState("http://localhost:2000");
+    const [userSocket, setUserSocket] = useState<any>(null);
+    
+    const startConnection = async () => {
+        const socketConnection = io(backendServiceLink + "/guest", {
+            transports: ["websocket"],
+        });
+
+        socketConnection.on("streamBack", (audioData: any) => {
+            console.log(audioData)
+            // queueAudioData(audioData);
+        });
+
+        setUserSocket(socketConnection);
+    };
+
     const handleSubmit = (data: { name: string; email: string }) => {
         console.log("Submitted Data:", data);
         setInterviewId('12345')
         setDetails({...data})
+        startConnection()
     };
 
     const onCancelCall = () =>{
